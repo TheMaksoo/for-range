d71c9bcf4690e91722a928cec97fac6d2d0e817b
Moheax <39918032+TheMaksoo@users.noreply.github.com>
2021-11-09 10:08:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#TIP: gebruik het boekje van PRB-II, zie H3, paragraaf 3 voor voorbeelden.
+
+#1 Schrijf een for-loop i.c.m. range() die de getallen van 0 t/m 4 toont.
+for i in range(5):
+  print(i)
+#2 Schrijf een for-loop i.c.m. range() die de getallen van 5 t/m 9 toont.
+for i in range(5, 10):
+  print(i)
+#3 Schrijf een for-loop i.c.m. range() die de getallen van 21 t/m 26 toont.
+for i in range(21,27):
+  print(i)
+#4 Gebruik onderstaande lijst in een for-loop, zodat alle items geprint worden.
+planeList = ["B737", "A380", "F16", "B767"]
+for plane in planeList:
+  print(plane)
+#5 Gebruik onderstaande lijst in een for-loop zodat alle items geprint worden:
+teacherList = ["Elton", "Michel", "Bart", "Ine", "Robin", "Corné", "Steven"]
+for teacher in teacherList:
+  print(teacher)
